8c93f98902d18690ddff2c7a23cdbd690bc47b6e
Xiake Zhu <zhuxiake2017@gmail.com>
2020-05-13 10:32:01 +0800

    Add addNode method

--- lists2/IntList.java
+++ lists2/IntList.java
@@ -1,61 +1,74 @@
 public class IntList {
-	public int first;
-	public IntList rest;
+    public int first;
+    public IntList rest;
 
-    /**Constructor. */
-	public IntList(int f, IntList r) {
-		first = f;
-		rest = r;
+    /** Constructor. */
+    public IntList(int f, IntList r) {
+        first = f;
+        rest = r;
     }
-    
-    /**Add two identical numbers in a row and make one larger
-     * node. Using recursion.    */
-    public void addAdjacent(){
-        if(rest==null)
+
+    /**
+     * Add two identical numbers in a row and make one larger node. Using recursion.
+     */
+    public void addAdjacent() {
+        if (rest == null)
             return;
-        if(first==rest.first){
+        if (first == rest.first) {
             first *= 2;
             rest = rest.rest;
             addAdjacent();
-        }
-        else{
+        } else {
             rest.addAdjacent();
         }
     }
 
-	/** Return the size of the list using... recursion! */
-	public int size() {
-		if (rest == null) {
-			return 1;
-		}
-		return 1 + this.rest.size();
-	}
+    /** Return the size of the list using... recursion! */
+    public int size() {
+        if (rest == null) {
+            return 1;
+        }
+        return 1 + this.rest.size();
+    }
 
-	/** Return the size of the list using no recursion! */
-	public int iterativeSize() {
-		IntList p = this;
-		int totalSize = 0;
-		while (p != null) {
-			totalSize += 1;
-			p = p.rest;
-		}
-		return totalSize;
-	}
+    /** Return the size of the list using no recursion! */
+    public int iterativeSize() {
+        IntList p = this;
+        int totalSize = 0;
+        while (p != null) {
+            totalSize += 1;
+            p = p.rest;
+        }
+        return totalSize;
+    }
 
-	/** Returns the ith item of this IntList. */
-	public int get(int i) {
-		if (i == 0) {
-			return first;
-		}
-		return rest.get(i - 1);
-	}
+    /** Returns the ith item of this IntList. */
+    public int get(int i) {
+        if (i == 0) {
+            return first;
+        }
+        return rest.get(i - 1);
+    }
 
-	public static void main(String[] args) {
-		IntList L = new IntList(3, null);
-		L = new IntList(2, L);
+    /**
+     * Add an node and "sqaure" the list using recursion.
+     */
+    public void addNode(int x) {
+        if (rest == null) {
+            rest = new IntList(first * first, null);
+            rest.rest = new IntList(x, null);
+            return;
+        } else {
+            IntList p = rest;
+            rest = new IntList(first * first, p);
+            rest.rest.addNode(x);
+        }
+    }
+
+    public static void main(String[] args) {
+        IntList L = new IntList(2, null);
         L = new IntList(1, L);
-        L = new IntList(1, L);
-        L.addAdjacent();
-		System.out.println(L.size());
-	}
+        L.addNode(5);
+        L.addNode(7);
+    }
 }
